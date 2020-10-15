e34435fd62f23224527a4524093e84ead7f1dcb0
Dominic Celestine <dominicelestine23@gmail.com>
2020-10-15 16:37:29 -0400

    started version control

---
+++ _dev/registration.php
@@ -0,0 +1,11 @@
+<h1>Registration</h1>
+
+
+
+<form action="registration" method="post">
+
+    <p>Enter your first name </p>
+    <input type="text" name="first_name"></p>
+
+    <p>Enter your last name</p>
+    <input type="text" name="last_name"></p>
